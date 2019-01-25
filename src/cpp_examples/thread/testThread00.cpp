1db008e672fb83cec1436d893e80a3a8fc32639b
chouer19 <xuec16@mails.tsinghua.edu.cn>
2019-01-25 17:54:21 +0800

    cpp thread examples

--- src/cpp_examples/thread/testThread00.cpp
+++ src/cpp_examples/thread/testThread00.cpp
@@ -6,15 +6,36 @@
 //This function will be called from a thread
 
 void call_from_thread() {
-    std::cout << "Hello, World!" << std::endl;
+    for(int i=0;i<10;i++){
+        std::cout << "1st Hello, World!" << std::endl;
+    }
+}
+
+void call_from_thread2() {
+    for(int i=0;i<10;i++){
+        std::cout << "2st Hello, World!" << std::endl;
+    }
 }
 
 int main() {
     //Launch a thread
     std::thread t1(call_from_thread);
 
+    //Launch a thread
+    std::thread t2(call_from_thread2);
+
+    for(int i=0;i<10;i++){
+        std::cout << "main Hello, World!" << std::endl;
+    }
+
     //Join the thread with the main thread
     t1.join();
+    t2.join();
     
+
+    for(int i=0;i<10;i++){
+        std::cout << "last Hello, World!" << std::endl;
+    }
+
     return 0;
 }
